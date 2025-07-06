88b5ba62f52ce20e78a7fc395c139e50a94ebc02
a6iyyu <mizukinako7@gmail.com>
2025-07-06 20:48:59 +0700

    :construction: Add script in MainMenu.cs

--- Assets/Scripts/Systems/MainMenu.cs
+++ Assets/Scripts/Systems/MainMenu.cs
@@ -3,25 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void Play()
     {
-        Debug.Log("Play Game button pressed");
-        SceneManager.LoadScene("Play");
+        Debug.Log("The play button is pressed.");
+        SceneManager.LoadScene("Prolog");
     }
 
-    public void ExitGame()
+    public void Settings()
     {
-        Debug.Log("Exit Game button pressed");
-        Application.Quit();
-
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        Debug.Log("The settings button is pressed.");
+        SceneManager.LoadScene("Settings");
     }
 
     public void Credits()
     {
-        Debug.Log("Credits button pressed");
+        Debug.Log("The credits button is pressed.");
         SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("The exit button is pressed.");
+        Application.Quit();
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
