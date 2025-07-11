fc2702a318917ebface76dcdce4ed8d2c6596f88
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-07-11 14:12:45 +0700

    feat : menambahkan Manuscript untuk mentrigger Player setelah membaca Manuscript maka dia akan terbawa di boss arena

--- Assets/Scripts/Collectibles/Manuscript.cs
+++ Assets/Scripts/Collectibles/Manuscript.cs
@@ -1,7 +1,49 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-namespace Assets.Scripts.Collectibles
+public class Manuscript : MonoBehaviour
 {
-    public class Manuscript : ScriptableObject
-    { }
+    [Header("UI")]
+    public GameObject dialogPanel; // Panel dialog (diaktifkan saat player dekat)
+    public Button okButton;        // Tombol OK untuk mengonfirmasi teleportasi
+
+    [Header("Teleport Settings")]
+    public Transform bossArenaSpawnPoint; // Target lokasi teleportasi
+
+    private bool playerInTrigger = false;
+    private GameObject player;
+
+    void Start()
+    {
+        dialogPanel.SetActive(false); // pastikan dialog awalnya mati
+        okButton.onClick.AddListener(OnOkClicked);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+            player = other.gameObject;
+            dialogPanel.SetActive(true); // tampilkan UI
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            dialogPanel.SetActive(false);
+        }
+    }
+
+    void OnOkClicked()
+    {
+        if (playerInTrigger && player != null)
+        {
+            player.transform.position = bossArenaSpawnPoint.position;
+            dialogPanel.SetActive(false);
+        }
+    }
 }
