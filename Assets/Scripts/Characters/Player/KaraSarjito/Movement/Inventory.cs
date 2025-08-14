27f4b199dbdb6f32a6f4506c29bd39a4d86a57c9
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-08-14 21:51:22 +0700

    Refactor: Remove unused inventory and item scripts, cleaning up the project structure

--- Assets/Scripts/Characters/Player/KaraSarjito/Movement/Inventory.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Inventory", menuName = "ScriptableObjects/Inventory")]
-public class Inventory : ScriptableObject
-{
-    public Item[] items = new Item[5]; // Hotbar dengan 5 slot
-}
